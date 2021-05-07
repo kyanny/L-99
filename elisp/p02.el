ff28bc85d5b8d298ad0e9150a83491117fcdbe14
Kensuke Nagae <kyanny@gmail.com>
2021-05-08 04:13:50 +0900

    Add p02.el

---
+++ elisp/p02.el
@@ -0,0 +1,23 @@
+;; P02 (*) Find the last but one box of a list.
+;; Example:
+;; * (my-but-last '(a b c d))
+;; (C D)
+
+(cdr '(c d))
+(cdr (cdr '(c d)))
+(cdr (cdr '(d)))
+
+(let ((list '(a b c d)))
+  (car list))
+
+(let ((list '(d)))
+  (if (eq (cdr (cdr list)) nil)
+      list
+    t))
+
+(defun my-but-last (list)
+  (if (eq (cdr (cdr list)) nil)
+      list
+    (my-but-last (cdr list))))
+
+(my-but-last '(a b c d))
